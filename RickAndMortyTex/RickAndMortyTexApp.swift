c76f07a2d84f40348bcc46e575524aca70eb7b6b
Amador Navarro <amador_navarro@icloud.com>
2023-01-31 17:28:16 +0100

    Added base project

---
+++ RickAndMortyTex/RickAndMortyTexApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTexApp.swift
+//  RickAndMortyTex
+//
+//  Created by Amador Navarro Lucas on 31/1/23.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyTexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
